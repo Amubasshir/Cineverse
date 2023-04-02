f84612e98a27ace64cc441fe13c325f448540acd
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-04-02 07:46:11 +0600

    page&component structure added and routing setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import PageNotFound from './pages/404/PageNotFound';
+import Details from './pages/details/Details';
+import Explore from './pages/explore/Explore';
+import Home from './pages/home/Home';
+import SearchResult from './pages/searchResult/SearchResult';
 import { getApiConfig } from './store/homeSlice';
 import { fetchDataFromApi } from './utils/api';
 
@@ -18,11 +26,17 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>welcome to cineverse</h1>
-
-      {url?.total_results}
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
