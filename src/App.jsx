d2eee19401112586a05d703dd0b7ab03fc58c1f7
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-04-02 07:14:34 +0600

    store added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getApiConfig } from './store/homeSlice';
 import { fetchDataFromApi } from './utils/api';
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+
   useEffect(() => {
     apiTesting();
   }, []);
 
   const apiTesting = () => {
     fetchDataFromApi('/movie/popular').then((res) => {
-      console.log(res);
+      dispatch(getApiConfig(res));
     });
   };
 
   return (
     <div className="App">
       <h1>welcome to cineverse</h1>
+
+      {url?.total_results}
     </div>
   );
 }
