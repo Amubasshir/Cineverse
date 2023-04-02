6fcfb108f3b885ad1fe7b129a0b14c40235a8816
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-04-02 08:12:21 +0600

    useFetch hook added

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './style.scss';
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  const [background, setBackground] = useState('');
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (event) => {
+    if (event.key === 'Enter' && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome</span>
+          <span className="subTitle">
+            Unlimited movies, TV shows, and more. Get all now!!!
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie, tv show, or more..."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
