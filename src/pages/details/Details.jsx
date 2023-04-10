7f3f082257daa59a8337c910578aee8fda0ee1ca
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-04-10 15:35:59 +0600

    directors and writers added to details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import DetailsBanner from './detailsBanner/DetailsBanner';
+import { useParams } from 'react-router-dom';
 import './style.scss';
+
+import useFetch from '../../hooks/useFetch';
+import DetailsBanner from './detailsBanner/DetailsBanner';
+
 const Details = () => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
+
   return (
     <div>
-      <DetailsBanner />
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
     </div>
   );
 };
