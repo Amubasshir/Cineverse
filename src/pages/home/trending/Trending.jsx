fb1da2401ef109b658edc8c01cbd6bb0a89909cc
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-04-10 10:18:48 +0600

    rating added to card

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import Carousel from '../../../components/carousel/Carousel';
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
 
@@ -8,7 +9,7 @@ import useFetch from '../../../hooks/useFetch';
 const Trending = () => {
   const [endpoint, setEndpoint] = useState('day');
 
-  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
+  const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
 
   const onTabChange = (tab) => {
     setEndpoint(tab === 'Today' ? 'day' : 'week');
@@ -20,6 +21,7 @@ const Trending = () => {
         <span className="carouselTitle">Trending</span>
         <SwitchTabs data={['Today', 'This Week']} onTabChange={onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
     </div>
   );
 };
